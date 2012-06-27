25a7fd1100f536e35aa123ccf73628fb0b348f8e
Alex Denisov <1101.debian@gmail.com>
2012-06-27 09:41:26 +0300

    Up/Down votes use Ajax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,30 +14,45 @@ module ApplicationHelper
   end
 
   def rating_badge(excerpt)
-    raw "<span class='badge rating #{rating_helper(excerpt.rating)}' data-id='#{excerpt.id}'>#{excerpt.rating}</span>"
+    raw_span("rating badge #{rating_helper(excerpt.rating)}", excerpt.id, excerpt.rating)
   end
 
   def vote_down_link(excerpt)
-    icon = raw("<i class='icon-thumbs-down'></i>")
     if current_user.disliked.include? excerpt
-      return icon
+      content = icon_down
+    else
+      content = vote_link(vote_down_excerpt_path(excerpt), icon_down)
     end
-    link_to icon,
-            vote_down_excerpt_path(excerpt), 
-            :id => "down_#{excerpt.id}", 
-            :remote => true, 
-            :method => :put
+    raw_span('down', excerpt.id, content)
   end
 
   def vote_up_link(excerpt)
-    icon = raw("<i class='icon-thumbs-up'></i>")
     if current_user.liked.include? excerpt
-      return icon
+      content = icon_up
+    else
+      content = vote_link(vote_up_excerpt_path(excerpt), icon_up)
     end
-    link_to icon,
-            vote_up_excerpt_path(excerpt), 
-            :id => "up_#{excerpt.id}", 
-            :remote => true, 
-            :method => :put
+    raw_span('up', excerpt.id, content)
   end
+
+  def raw_span(span_class, data_id, content)
+    raw "<span class='#{span_class}' data-id='#{data_id}'>#{content}</span>"
+  end
+
+  def icon_down
+    icon 'down'
+  end
+  
+  def icon_up
+    icon 'up'
+  end
+
+  def icon(type)
+    "<i class='icon-thumbs-#{type}'></i>"
+  end
+
+  def vote_link(url, icon)
+    "<a href=#{url} data-method='put' data-remote='true'>#{icon}</a>"
+  end
+
 end
--- app/views/excerpts/rating.js.coffee
+++ app/views/excerpts/rating.js.coffee
@@ -1,8 +1,5 @@
 $('span.rating[data-id="<%= @excerpt.id %>"]').replaceWith "<%= rating_badge(@excerpt) %>"
-vote_down = "a#down_<%= @excerpt.id %>"
-alert "<%= vote_down_link(@excerpt) %>"
-$(vote_down).replaceWith "<%= vote_down_link(@excerpt) %>"
-vote_up = "a#up_<%= @excerpt.id %>"
-$(vote_up).replaceWith "<%= vote_up_link(@excerpt) %>"
+$('span.down[data-id="<%= @excerpt.id %>"]').replaceWith "<%= vote_down_link(@excerpt) %>"
+$('span.up[data-id="<%= @excerpt.id %>"]').replaceWith "<%= vote_up_link(@excerpt) %>"
 
 
